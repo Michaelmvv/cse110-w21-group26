364b4d31dd91b5eb66f66ed31f92a80d03e4cb81
Michael Maddy <Michaelmvv@users.noreply.github.com>
2021-02-10 15:54:53 -0800

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,9 @@ window.onload = () => {
   setInterval(update, 1000);
 };
 
-
+;
+;
+;
 
 // Exit Prompt... Only works if you have interacted with site...
 // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
