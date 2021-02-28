48b2e0c39428270643a26f3ed89b0a94aafe347d
Parth Patel <parthpatel2019@gmail.com>
2021-02-27 19:23:16 -0800

    Added Two List Functionality

--- task-item.js
+++ task-item.js
@@ -1,6 +1,7 @@
 /* global saveList, displayList */
 /**Variable storing the list of tasks as a JSON object */
 let taskList = [];
+let taskListDone = [];
 class taskEntry extends HTMLElement {
   /**
    * Constructor for the taskEntry HTMLElement, containing the name, session
@@ -40,8 +41,11 @@ class taskEntry extends HTMLElement {
       </style>
       <span>
       <li class="object">
+        <button onclick="" id="upTask">Up</button>
+        <button onclick="" id="downTask">Down</button>
         <p class="Name" id="name">Task Name</p>
-        <p class="Session" id="session"> 0 </p>
+        <input id="taskSessionNumber" name="taskSessionNumber" type="number" min="1" max="10" value="1">
+        <button onclick="" id="moveToNewList">Switch List</button>
         <button onclick="" id="removeTask">Remove Task</button>
       </li>
       </span>
@@ -58,13 +62,77 @@ class taskEntry extends HTMLElement {
   async syncName(description) {
     let taskName = this.root.getElementById('name');
     taskName.textContent = description.name;
-    let sessionCount = this.root.getElementById('session');
-    sessionCount.textContent = description.sessions;
-    let button = this.root.getElementById('removeTask');
-    button.addEventListener('click', function() {
+    let sessionCount = this.root.getElementById('taskSessionNumber');
+    sessionCount.addEventListener('change', function() {
+      updateStorage(taskName.textContent, sessionCount.value)
+    })
+    sessionCount.value = description.sessions;
+    let buttonRemove = this.root.getElementById('removeTask');
+    buttonRemove.addEventListener('click', function() {
       removeButton(this, taskName.textContent);
     });
+    let buttonSwitch = this.root.getElementById('moveToNewList');
+    buttonSwitch.addEventListener('click', function() {
+      switchList(this, taskName.textContent);
+    });
+    let buttonUp = this.root.getElementById('upTask');
+    buttonUp.addEventListener('click', function() {
+      switchOrder(this, taskName.textContent, true);
+    });
+    let buttonDown = this.root.getElementById('downTask');
+    buttonDown.addEventListener('click', function() {
+      switchOrder(this, taskName.textContent, false);
+    });
   }
+}
+
+/**
+ *  Updates local storage with new value of remaining sessions count
+ * 
+ * @param {string} name - Name of Task
+ * @param {int} newCount - New remaining sessions count
+ * 
+ */
+function updateStorage(name, newCount) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (name == taskList[i].name) {
+      taskList[i].sessions = newCount;
+      break;
+    }
+  }
+  saveList();
+  displayList();
+}
+
+/**
+ * Function called to switch order of tasks in task list implementation
+ * @param {HTMLElement} button - The HTML button object this function is being
+ *     attached to
+ * @param {string} name - The name of the list entry that is being removed
+ * @param {string} upDown - Whether the selected task will be moved up or down
+ * 
+ */
+function switchOrder(button, name, upDown) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (name == taskList[i].name) {
+      if(upDown) {
+        if(i != 0) {
+          var current = taskList[i];
+          taskList[i] = taskList[i-1];
+          taskList[i-1] = current;
+        }
+      } else {
+        if(i != taskList.length - 1) {
+          current = taskList[i];
+          taskList[i] = taskList[i+1];
+          taskList[i+1] = current;
+        }
+      }
+      break;
+    }
+  }
+  saveList();
+  displayList();
 }
 
 /**
@@ -74,10 +142,9 @@ class taskEntry extends HTMLElement {
  *     attached to
  * @param {string} name - The name of the list entry that is being removed
  */
-
 function removeButton(button, name) {
   removeTask(name);
-  console.log('Remove');
+  console.log('Removed ' + name);
 }
 
 /**
@@ -85,9 +152,9 @@ function removeButton(button, name) {
  * @constructor
  * @param {string} name - Name of Task
  */
-function Task(name) {
+function Task(name, sessionCount) {
   this.name = name;
-  this.sessions = 0;
+  this.sessions = sessionCount;
   this.done = false;
 }
 
@@ -95,12 +162,44 @@ function Task(name) {
  * Create the Task Object and add it to list/save in local storage
  * @param {string} name - Name of Task
  */
-function putTaskInList(name) {
-  let t = new Task(name);
+function putTaskInList(name, sessionCount) {
+  let t = new Task(name, sessionCount);
   taskList.push(t);
   saveList();
   displayList();
 }
+
+/**
+ * 
+ * @param {*} name 
+ */
+
+ function switchList(button, name) {
+   for (let i = 0; i < taskList.length; i++) {
+    if (name == taskList[i].name) {
+      taskList.done = true;
+      button.innerHTML = 'Move to Done';
+      taskListDone.push(taskList[i]);
+      taskList.splice(i, 1);
+      saveList();
+      displayList();
+      return;
+    }
+  }
+  for (let i = 0; i < taskListDone.length; i++) {
+    if (name == taskListDone[i].name) {
+      taskList.done = false;
+      button.innerHTML = "Move to To-Do";
+      taskList.push(taskListDone[i]);
+      taskListDone.splice(i, 1);
+      saveList();
+      displayList();
+      return;
+    }
+  }
+ }
+
+
 
 /**
  * Remove the task object by list
@@ -122,8 +221,21 @@ function removeTask(name) {
  */
 function addTask() {
   var name = document.getElementById('addTaskInput').value;
+  for (let i = 0; i < taskList.length; i++) {
+    if (name == taskList[i].name) {
+      alert("You cannot have the same name as a previous task");
+      return;
+    }
+  }
+  for (let i = 0; i < taskListDone.length; i++) {
+    if (name == taskListDone[i].name) {
+      alert("You cannot have the same name as a previous task");
+      return;
+    }
+  }
+  var sessionCount = document.getElementById('sessionNumber').value;
   if (name != null) {
-    putTaskInList(name);
+    putTaskInList(name, sessionCount);
   }
 }
 
@@ -131,10 +243,15 @@ function addTask() {
  * Accesses local storage to populate taskList and sets to empty if not found
  */
 function getList() {
-  if (localStorage.getItem('taskList-JSON') != null) {
-    taskList = JSON.parse(localStorage.getItem('taskList-JSON'));
+  if (localStorage.getItem('taskListCurrent-JSON') != null) {
+    taskList = JSON.parse(localStorage.getItem('taskListCurrent-JSON'));
   } else {
     taskList = [];
+  }
+  if (localStorage.getItem('taskListDone-JSON') != null) {
+    taskListDone = JSON.parse(localStorage.getItem('taskListDone-JSON'));
+  } else {
+    taskListDone = [];
   }
 }
 
@@ -142,18 +259,33 @@ function getList() {
  * Save taskList in a string version to local storage
  */
 function saveList() {
-  localStorage.setItem('taskList-JSON', JSON.stringify(taskList));
+  localStorage.setItem('taskListCurrent-JSON', JSON.stringify(taskList));
+  localStorage.setItem('taskListDone-JSON', JSON.stringify(taskListDone));
 }
 
 /**
- * Displays all the task list on the web page
+ * Displays all the task list current on the web page
  */
 function displayList() {
   var taskFile = document.getElementById('tasks');
-  console.log(taskList);
+  // console.log(taskList);
   taskFile.innerHTML = '';
   for (let i = 0; i < taskList.length; i++) {
     var currentTask = taskList[i];
+    let newTask = new taskEntry(); //So code factor is happy
+    newTask.syncName(currentTask);
+    taskFile.appendChild(newTask);
+  }
+}
+/**
+ * Displays all the task list done on the web page
+ */
+function displayListDone() {
+  var taskFile = document.getElementById('tasks');
+  // console.log(taskList);
+  taskFile.innerHTML = '';
+  for (let i = 0; i < taskListDone.length; i++) {
+    var currentTask = taskListDone[i];
     let newTask = new taskEntry(); //So code factor is happy
     newTask.syncName(currentTask);
     taskFile.appendChild(newTask);
